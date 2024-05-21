d79e110bfb840a1d8be82b475d70bd425c317714
Carla Mariana <carlamab2808@outlook.com>
2024-05-20 23:54:01 -0300

    .

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,7 +12,7 @@ export class ListarPensamentoComponent implements OnInit {
       conteudo: 'Passo informações para o componente filho',
       autoria: 'Componente pai',
       modelo: 'modelo3'
-    }
+    },
     {
       conteudo: 'Minha propriedade é decorada com @Input()',
       autoria: 'Componente filho',
